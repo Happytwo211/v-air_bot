ceb0c6d6a91fd1a2bb73a8a52e3240d8bf1231e0
DanielDitrikh <daniel.ditrikh@mail.ru>
2025-01-07 00:55:44 +0300

    07.01

--- DB/groups_db_commands.py
+++ DB/groups_db_commands.py
@@ -3,6 +3,16 @@ import sqlite3
 connection = sqlite3.connect('Groups.db',check_same_thread=False)
 cursor = connection.cursor()
 
+#Команды:
+
+# cursor.execute(
+#     'DELETE FROM Groups WHERE Group_name == ?',(Group_name,)
+# )
+# cursor.execute(
+#     'DELETE FROM Groups WHERE Group_name == ?',(Group_name,)
+# )
+
+
 class TableGroupsDB:
     pass
     cursor.execute('''
@@ -15,7 +25,6 @@ class TableGroupsDB:
     ''')
 
 class AddValue:
-
     def add_table_value(Group_Name, Week, Day, Year):
         print(
             f'Вы хотите внести в таблицу:'
@@ -42,29 +51,8 @@ class AddValue:
         if user_input == 'n':
             print('Данные не внесены')
 
-    def add_group_name(Group_Name):
-        cursor.execute(
-            'INSERT INTO Groups (Group_Name) VAlUES (?)', (Group_Name,)
-        )
-        connection.commit()
-    def add_week(week):
-        cursor.execute(
-            'INSERT INTO Groups (Week) VAlUES (?)', (week,)
-        )
-        connection.commit()
-    def add_day(Day):
-        cursor.execute(
-            'INSERT INTO Groups (Day) VAlUES (?)', (Day,)
-        )
-        connection.commit()
-    def add_year(Year):
-        cursor.execute(
-            'INSERT INTO Groups (Year) VAlUES (?)', (Year,)
-        )
-        connection.commit()
 
 class List:
-
     @staticmethod
     def list_table():
       cursor.execute('SELECT * FROM Groups')
@@ -74,36 +62,5 @@ class List:
         return
 
 
-class Delete:
-
-
-
-    def delete_group_name(Group_name):
-        cursor.execute(
-            'DELETE FROM Groups WHERE Group_name == ?',(Group_name,)
-        )
-        connection.commit()
-
-    def delete_week(Week):
-        cursor.execute(
-            'DELETE FROM Groups WHERE Week == ?', (Week,)
-        )
-        connection.commit()
-
-    def delete_day(Day):
-        cursor.execute(
-            'DELETE FROM Groups WHERE Day == ?',(Day,)
-        )
-        connection.commit()
-
-    def delete_year(Year):
-        cursor.execute(
-            'DELETE FROM Groups WHERE Year == ?', (Year,)
-        )
-        connection.commit()
-
-
-
-# List.list_table()
 connection.commit()
 connection.close()
