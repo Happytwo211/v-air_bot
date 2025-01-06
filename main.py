621fb09cbef346c53b79b7826205e93f997f0829
DanielDitrikh <daniel.ditrikh@mail.ru>
2025-01-06 06:46:08 +0300

    added classes

--- main.py
+++ main.py
@@ -16,28 +16,24 @@ cursor = connection.cursor()
 bot = telebot.TeleBot(Token.TOKEN)
 admins = admin_list.admin_id
 
-#Start Keyboard
-inline_keyboard_student_admin = types.InlineKeyboardMarkup(row_width=2)
-inline_keyboard_student_admin_button_1 = types.InlineKeyboardButton('Я ученик', callback_data='student')
-inline_keyboard_student_admin_button_2 = types.InlineKeyboardButton('Я преподаватель', callback_data='tutor')
-inline_keyboard_student_admin.add(inline_keyboard_student_admin_button_1).add(inline_keyboard_student_admin_button_2)
-
-#Student Keyboard
-inline_keyboard_student = types.InlineKeyboardMarkup()
-inline_keyboard_student_button_1 = types.InlineKeyboardButton('Узнать расписание cвоей группы', callback_data='schedule_student')
-inline_keyboard_student_button_2 = types.InlineKeyboardButton('Получить материалы уроков', callback_data='lesson_materials_student')
-inline_keyboard_student_button_3 = types.InlineKeyboardButton('Вернуться в главное меню', callback_data='main_menu')
-inline_keyboard_student.add(inline_keyboard_student_button_1).add(inline_keyboard_student_button_2).add(inline_keyboard_student_button_3)
-
-
-
-class Commands:
-    @bot.message_handler(commands=['start'])
-    def handle_start(message):
-        bot.send_photo(message.chat.id, 'https://imgur.com/a/4kg6OIp',
-                       f'Это бот проекта ~V-Air~'
-                       f'\n', reply_markup=inline_keyboard_student_admin)
-
+class StartKeyboard:
+    @staticmethod
+    def show_start_kb():
+        inline_keyboard_student_admin = types.InlineKeyboardMarkup(row_width=2)
+        inline_keyboard_student_admin_button_1 = types.InlineKeyboardButton('Я ученик', callback_data='student')
+        inline_keyboard_student_admin_button_2 = types.InlineKeyboardButton('Я преподаватель', callback_data='tutor')
+        inline_keyboard_student_admin.add(inline_keyboard_student_admin_button_1).add(inline_keyboard_student_admin_button_2)
+        return inline_keyboard_student_admin
+class StudentKeyboard:
+    @staticmethod
+    def show_student_kb():
+        inline_keyboard_student = types.InlineKeyboardMarkup()
+        inline_keyboard_student_button_1 = types.InlineKeyboardButton('Узнать расписание cвоей группы', callback_data='schedule_student')
+        inline_keyboard_student_button_2 = types.InlineKeyboardButton('Получить материалы уроков', callback_data='lesson_materials_student')
+        inline_keyboard_student_button_3 = types.InlineKeyboardButton('Вернуться в главное меню', callback_data='main_menu')
+        inline_keyboard_student.add(inline_keyboard_student_button_1).add(inline_keyboard_student_button_2).add(inline_keyboard_student_button_3)
+        return inline_keyboard_student
+class DB:
     @bot.message_handler(commands=['db'])
     def db_test(message):
         cursor.execute('SELECT * FROM Groups')
@@ -45,11 +41,24 @@ class Commands:
         data_message = "\n".join([f"{row}" for row in rows])
         bot.send_message(message.chat.id, f'Вот твое расписание\n {data_message}')
 
+        inline_keyboard_db_choose_group = types.InlineKeyboardMarkup(row_width=2)
+        inline_keyboard_db_button_1 = types.InlineKeyboardButton('Группа-1', callback_data='gr-1')
+        inline_keyboard_db_button_2 = types.InlineKeyboardButton('Группа-2', callback_data='gr-2')
+        inline_keyboard_db_choose_group.add(inline_keyboard_db_button_1).add(inline_keyboard_db_button_2)
+        bot.send_message(message.chat.id, f'Выбери свою группу', reply_markup=inline_keyboard_db_choose_group)
+
+class Commands:
+    @bot.message_handler(commands=['start'])
+    def handle_start(message):
+        bot.send_photo(message.chat.id, 'https://imgur.com/a/4kg6OIp',
+                       f'Это бот проекта ~V-Air~'
+                       f'\n', reply_markup=StartKeyboard.show_start_kb())
+
 class StudentCallBackData:
     @bot.callback_query_handler(func=lambda call: call.data == 'student')
     def student(call):
         message = call.message
-        bot.send_message(message.chat.id, f'Чем бот может тебе помочь?', reply_markup=inline_keyboard_student)
+        bot.send_message(message.chat.id, f'Чем бот может тебе помочь?', reply_markup=StudentKeyboard.show_student_kb())
 
     @bot.callback_query_handler(func=lambda call: call.data == 'schedule_student')
     def schedule_student(call):
@@ -64,10 +73,18 @@ class StudentCallBackData:
     @bot.callback_query_handler(func=lambda call: call.data == 'main_menu')
     def schedule_student(call):
         message = call.message
-        bot.send_message(message.chat.id, f'Вы вернулись в главнео меню',reply_markup=inline_keyboard_student_admin)
+        bot.send_message(message.chat.id, f'Вы вернулись в главнео меню',reply_markup=StudentKeyboard.show_student_kb())
+
+    @bot.callback_query_handler(func=lambda call: call.data == 'gr-1')
+    def schedule_student(call):
+        message = call.message
+        db_data = cursor.execute('SELECT * FROM Groups ORDER BY "Group_Name"')
+        bot.send_message(message.chat.id, f'Расписание группы-1\n{(db_data)}')
 
 class TutorCallBackData:
     pass
+
+
 
 if __name__ == '__main__':
     bot.infinity_polling()
